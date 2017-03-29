73c128f23c19b2ed8652704bb997f2c80cacbd17
khinezarthwe <koalakhine@gmail.com>
2017-03-29 15:50:56 +0900

    Fix test and update nav var with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = User.create(username: "John",email: "john@example.com",password: "password", admin: true)
+	end
 
 	test "get new categories form and create category" do
+		sign_in_as(@user,"password")
 		get new_category_path
 		assert_template 'categories/new'
 		post categories_path, params: {category: {name: "sports"}}
@@ -15,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 	end
 
 	test "invalid category submission results in failure" do
+		sign_in_as(@user,"password")
 		get new_category_path
 		assert_template 'categories/new'
 		assert_no_difference 'Category.count' do
